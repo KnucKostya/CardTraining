d43091320617667fdb36cb7616ef687c92887e40
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-06-13 14:15:22 +0500

    upd: add/edit  selectors

---
+++ src/features/packs/packs.selector.ts
@@ -0,0 +1,7 @@
+import { RootState } from "../../app/store";
+
+export const packs_Selector=(state: RootState)=> state.packs.cardPacks
+export const minCardsCount_Selector=(state: RootState)=> state.packs.minCardsCount
+export const maxCardsCount_Selector=(state: RootState)=> state.packs.maxCardsCount
+export const packsCount_Selector =(state: RootState) => state.packs.cardPacksTotalCount
+
