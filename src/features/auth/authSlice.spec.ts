61e40638d4220951a3e40794a94b4d738ca755cf
My Name <knuckostya@gmail.com>
2023-09-15 23:24:49 +0300

    tested all required thunks in auth thunks

--- src/features/auth/authSlice.spec.ts
+++ src/features/auth/authSlice.spec.ts
@@ -1,36 +1,37 @@
 import { authReducer, authThunks } from "./authSlice";
-import { ProfileType } from "./authApi";
+import { ProfileType } from "features/auth/authApi";
 
+let profile: ProfileType;
+beforeEach(() => {
+  profile = {
+    _id: "6435620aaf58963e887fb0f4",
+    email: "safrondev1@gmail.com",
+    rememberMe: false,
+    isAdmin: false,
+    name: "safrondev1@gmail.com",
+    verified: false,
+    publicCardPacksCount: 3,
+    created: new Date("2023-04-11T13:35:06.046Z"),
+    updated: new Date("2023-05-05T06:35:21.310Z"),
+    // __v: 0,
+    // token: "023f67e0-eb0f-11ed-b359-fbf835b5a380",
+    // tokenDeathTime: 1683279321310,
+  };
+});
 
 describe("authReducer", () => {
-  const initialState  = {
+  const initialState = {
     profile: null as ProfileType | null,
     isAuth: false,
     email: "",
-    password: ""
+    password: "",
   };
-
 
   it("should login work correctly and return profile", () => {
     const data = {
       email: "safrondev1@gmail.com",
       password: "1qazxcvBG",
       rememberMe: false,
-    };
-
-    const profile = {
-      _id: "6435620aaf58963e887fb0f4",
-      email: "safrondev1@gmail.com",
-      rememberMe: false,
-      isAdmin: false,
-      name: "safrondev1@gmail.com",
-      verified: false,
-      publicCardPacksCount: 3,
-      created: new Date("2023-04-11T13:35:06.046Z"),
-      updated: new Date("2023-05-05T06:35:21.310Z"),
-      __v: 0,
-      token: "023f67e0-eb0f-11ed-b359-fbf835b5a380",
-      tokenDeathTime: 1683279321310,
     };
 
     // 2. fulfilled takes 3 params:
@@ -43,5 +44,20 @@ describe("authReducer", () => {
     const state = authReducer(initialState, action);
 
     expect(state.profile).toEqual(profile);
+  });
+
+  it("should delete profile data and auth flag should be false when logoutTC is called", () => {
+    const action = authThunks.logoutTC.fulfilled({ info: "a", error: "null" }, "requestId");
+    const state = authReducer(initialState, action);
+    expect(state.profile).toEqual(null);
+    expect(state.isAuth).toEqual(false);
+  });
+  it("should set new password in setNewPassword", () => {
+    const action = authThunks.setNewPassword.fulfilled({}, "requestId", {
+      password: "new",
+      resetPasswordToken: "",
+    });
+    const state = authReducer(initialState, action);
+    expect(state.password).toEqual("new");
   });
 });
